295d3c807e57dc33ac479c380fedd90af75eddad
Muhammad Hussain <muhammadhussain@muhammads-mbp.home>
2024-09-20 23:41:50 +0100

    MongoDB installation and setup

--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import clientRoutes from './routes/client.js'
-import generalRoutes from './routes/general.js'
-import managementRoutes from './routes/management.js'
-import salesRoutes from './routes/sales.js'
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -26,3 +26,12 @@ app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9001;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
