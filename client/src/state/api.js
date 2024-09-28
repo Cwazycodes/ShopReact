fc52033a5f704d2ab1fae4ec34f775534dc48004
Muhammad Hussain <muhammadhussain@muhammads-mbp.home>
2024-09-28 23:31:27 +0100

    Overview page

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -5,7 +5,7 @@ export const api = createApi({
     baseUrl: process.env.REACT_APP_BASE_URL,
   }),
   reducerPath: "adminApi",
-  tagTypes: ["User", "Products", "Customers", "Transactions", "Geography"],
+  tagTypes: ["User", "Products", "Customers", "Transactions", "Geography", "Sales"],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
@@ -29,7 +29,11 @@ export const api = createApi({
     }),
     getGeography: build.query({
       query: () => "client/geography",
-      providesTags: ["Geography"]
+      providesTags: ["Geography"],
+    }),
+    getSales: build.query({
+      query: () => "sales/sales",
+      providesTags: ["Sales"]
     })
   }),
 });
@@ -39,5 +43,6 @@ export const {
   useGetProductsQuery,
   useGetCustomersQuery,
   useGetTransactionsQuery,
-  useGetGeographyQuery
+  useGetGeographyQuery,
+  useGetSalesQuery
 } = api;
